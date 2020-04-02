d208d6ef89a349d622b2139c6e693d0fa7212e28
Aesthetic <>
2020-04-03 04:47:13 +0800

    TEST：增加类，修改InputViewController的背景色

---
+++ ofo-demo/EmptyObject.h
@@ -0,0 +1,17 @@
+//
+//  EmptyObject.h
+//  ofo-demo
+//
+//  Created by Aesthetic on 2020/4/3.
+//  Copyright © 2020 Comin Bril. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EmptyObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ofo-demo/EmptyObject.m
@@ -0,0 +1,13 @@
+//
+//  EmptyObject.m
+//  ofo-demo
+//
+//  Created by Aesthetic on 2020/4/3.
+//  Copyright © 2020 Comin Bril. All rights reserved.
+//
+
+#import "EmptyObject.h"
+
+@implementation EmptyObject
+
+@end
