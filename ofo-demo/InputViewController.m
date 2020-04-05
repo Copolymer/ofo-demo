a34e8dae3edb1514b84e6b860d0acf5de6b9e3cb
Aesthetic <>
2020-04-06 04:06:08 +0800

    【变更】修改InputViewController

--- ofo-demo/InputViewController.m
+++ ofo-demo/InputViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view.
     self.title = @"验证码输入VC";
     self.view.backgroundColor = [UIColor redColor];
+    self.view.userInteractionEnabled = NO;
 }
 
 - (void)didReceiveMemoryWarning {
